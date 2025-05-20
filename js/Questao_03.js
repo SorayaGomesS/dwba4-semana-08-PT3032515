a21c26a5c23c8c80fb15f7ac8ead03704d448716
SorayaGomesS <sorayagomes62@gmail.com>
2025-05-20 20:08:32 -0300

    Create Questao_03.js

---
+++ js/Questao_03.js
@@ -0,0 +1,29 @@
+var nome = "Soraya Gomes";
+var primeiraNota = 7;
+var segundaNota = 8.5;
+var multPrimeiraNota = primeiraNota * 0.6;
+var multSegundaNota = segundaNota * 0.4;
+var soma = multPrimeiraNota + multSegundaNota;
+
+alert("Nome do aluno: " + nome + "\nNota 1: " + primeiraNota + "\nNota 2: " + segundaNota);
+alert("Nome do aluno: " + nome + "\nNota total: " + soma);
+
+nome = "Mari Vitoria";
+primeiraNota = 10;
+segundaNota = 9;
+multPrimeiraNota = primeiraNota * 0.6;
+multSegundaNota = segundaNota * 0.4;
+soma = multPrimeiraNota + multSegundaNota;
+
+alert("Nome do aluno: " + nome + "\nNota 1: " + primeiraNota + "\nNota 2: " + segundaNota);
+alert("Nome do aluno: " + nome + "\nNota total: " + soma);
+
+nome = "Amanda Maciel";
+primeiraNota = 8;
+segundaNota = 9;
+multPrimeiraNota = primeiraNota * 0.6;
+multSegundaNota = segundaNota * 0.4;
+soma = multPrimeiraNota + multSegundaNota;
+
+alert("Nome do aluno: " + nome + "\nNota 1: " + primeiraNota + "\nNota 2: " + segundaNota);
+alert("Nome do aluno: " + nome + "\nNota total: " + soma);
