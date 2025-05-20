dc7af17f3c881c12672257ae10a766e35308288a
SorayaGomesS <sorayagomes62@gmail.com>
2025-05-20 20:08:57 -0300

    Create Questao_04.js

---
+++ js/Questao_04.js
@@ -0,0 +1,36 @@
+class Aluno {
+    constructor(primeiroNome, segundoNome, primeiraNota, segundaNota) {
+        this.primeiroNome = primeiroNome;
+        this.segundoNome = segundoNome;
+        this.primeiraNota = primeiraNota;
+        this.segundaNota = segundaNota;
+    }
+
+    nomeCompleto() {
+        let nomeCompleto = this.primeiroNome + " " + this.segundoNome;
+        return nomeCompleto;
+    }
+
+    media() {
+        return (this.primeiraNota * 0.6) + (this.segundaNota * 0.4);
+    }
+
+    situacao() {
+        if (this.media() > 6) {
+            return "Aprovado";
+        } else {
+            return "Reprovado";
+        }
+    }
+}
+
+var alunos = new Array(5);
+alunos[0] = new Aluno("Soraya", "Gomes", 6, 8);
+alunos[1] = new Aluno("Mari", "Vitoria", 10, 8);
+alunos[2] = new Aluno("Amanda", "Maciel", 5, 7);
+alunos[3] = new Aluno("Gustavo", "Maximo", 5, 3);
+alunos[4] = new Aluno("Gesilda", "Gomes", 6, 7);
+
+for (var i = 0; i < 5; i++) {
+    alert("Nome completo: " + alunos[i].nomeCompleto() + "\nMédia: " + alunos[i].media() + "\nSituação: " + alunos[i].situacao());
+}
